87bed09ce4387e1a5bc235b762b6bd39e7280156
ed-webdev <eduardreactdev@gmail.com>
2020-11-22 15:55:55 +0300

    add warehouse

--- src/components/lists/warehouses/createWarehouse.jsx
+++ src/components/lists/warehouses/createWarehouse.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {connect} from "react-redux";
 
 import SwitchPage from "../../SwitchPage";
@@ -7,26 +7,58 @@ import Col from "../../basic/col";
 import Button from "../../basic/button";
 import List from "./list";
 
+import validator from "../../../helpers/validation";
+
 const CreateWarehouse = (props) => {
+    const [name, setName] = useState('')
+    const [address, setAddress] = useState('')
+    const [products, setProducts] = useState([])
     const OwnerWarehouse = props.warehouses.length > 0 && props.warehouses.filter(elem => elem.name === 'Нераспределенный склад 1')
+
+    const createWarehouse = () => {
+        if (validator(name) && validator(address)) {
+            //если сохранился, удалять эти продукты из нерапределенного склада
+            return props.createWarehouse({
+                name,
+                address,
+                id: Date.now(),
+                products
+            })
+        }
+    }
+
+    const onAdd = (item) => {
+        if (products.filter(elem => elem.id === item.id).length > 0) {
+            setProducts(products.map(elem => {
+                if (elem.id === item.id) {
+                    elem = item
+                }
+                return elem
+            }))
+        } else {
+            setProducts(products.concat(item))
+        }
+        console.log(products)
+    }
 
     return (
         <>
             <SwitchPage/>
             <Row styles="centerRow">
                 <Col styles="input-field s4">
-                    <input type="text"/>
+                    <input type="text" onChange={(e) => setName(e.target.value)}/>
                     <label>Название</label>
                 </Col>
                 <Col styles="input-field s4">
-                    <input type="text"/>
+                    <input type="text" onChange={(e) => setAddress(e.target.value)}/>
                     <label>Адрес</label>
                 </Col>
                 <Col styles="s4">
-                    <Button>Создать</Button>
+                    <Button onClick={() => createWarehouse()}>Создать</Button>
                 </Col>
             </Row>
-            {OwnerWarehouse.length > 0 && <List items={OwnerWarehouse[0].products} type="add" title="Вы можете добавить товары из нераспределенного склада в этот склад"/>}
+            {OwnerWarehouse.length > 0 && <List items={OwnerWarehouse[0].products} type="add" onAdd={onAdd}
+                                                title="Вы можете добавить товары из нераспределенного склада в этот склад"/>}
         </>
     )
 }
@@ -39,6 +71,9 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
+        createWarehouse: (value) => {
+            dispatch({type: 'ADD_WAREHOUSE', value})
+        }
     }
 }
 
--- src/components/lists/warehouses/list.jsx
+++ src/components/lists/warehouses/list.jsx
@@ -1,56 +1,82 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {isMobile} from "react-device-detect";
 
 import Row from "../../basic/row";
 import Col from "../../basic/col";
 import Button from "../../basic/button";
 
-const List = (props) => (
-    <>
-        {props.title && <h5>{props.title}</h5>}
-        <div className={isMobile ? "main" : ''}>
-            <Row styles="titles">
-                <Col styles="s3">
-                    <span>Название</span>
-                </Col>
-                <Col styles="s3">
-                    <span>Количество на складе</span>
-                </Col>
-                <Col styles="s3">
-                    <span>Убрать со склада (данное действие приведет к переносу товара в нераспределенный склад)</span>
-                </Col>
-                <Col styles="s3">
-                    <span>Действие</span>
-                </Col>
-            </Row>
-            <ul className="collection">
-                {props.items.map((item, index) => (
-                    <li className="collection-item" key={index}>
-                        <Row>
-                            <Col styles="s3">
-                                <Row>
-                                    <span>{item.name}</span>
-                                </Row>
-                            </Col>
-                            <Col styles="s3">
-                                <Row>
-                                    <span>{item.quantity}</span>
-                                </Row>
-                            </Col>
-                            <Col styles="s3">
-                                <input type="number" id={`quantityProduct${index}`} min={0}
-                                       defaultValue={item.quantity}/>
-                                <label>всего на складе: {item.quantity}</label>
-                            </Col>
-                            <Col styles="s3">
-                                <Button>{props.type === 'add' ? 'Добавить' : 'Убрать'}</Button>
-                            </Col>
-                        </Row>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    </>
-)
+const List = (props) => {
+    const [products, setProducts] = useState([])
+
+    const selectQuantity = (item, value) => {
+        if (products.length > 0 && products.filter(elem => elem.id === item.id).length > 0) {
+            setProducts(products.map(elem => {
+                if (elem.id === item.id) {
+                    elem = {...elem, quantity: value}
+                }
+
+                return elem
+            }))
+        } else {
+            setProducts(products.concat({
+                ...item, quantity: value
+            }))
+        }
+    }
+
+    return (
+        <>
+            {props.title && <h5>{props.title}</h5>}
+            <div className={isMobile ? "main" : ''}>
+                <Row styles="titles">
+                    <Col styles="s3">
+                        <span>Название</span>
+                    </Col>
+                    <Col styles="s3">
+                        <span>Количество на складе</span>
+                    </Col>
+                    <Col styles="s3">
+                        <span>Убрать со склада (данное действие приведет к переносу товара в нераспределенный склад)</span>
+                    </Col>
+                    <Col styles="s3">
+                        <span>Действие</span>
+                    </Col>
+                </Row>
+                <ul className="collection">
+                    {props.items.map((item, index) => (
+                        <li className="collection-item" key={index}>
+                            <Row>
+                                <Col styles="s3">
+                                    <Row>
+                                        <span>{item.name}</span>
+                                    </Row>
+                                </Col>
+                                <Col styles="s3">
+                                    <Row>
+                                        <span>{item.quantity}</span>
+                                    </Row>
+                                </Col>
+                                <Col styles="s3">
+                                    <input type="number" id={`quantityProduct${index}`} min={1} max={item.quantity}
+                                           defaultValue={0}
+                                           onChange={(e) => selectQuantity(item, e.target.value)}/>
+                                    <label>всего на складе: {item.quantity}</label>
+                                </Col>
+                                <Col styles="s3">
+                                    <Button
+                                        onClick={props.type === 'add' ? () => props.onAdd(products.filter(elem => elem.id === item.id).pop()) : ''}
+                                        disabled={!products.filter(elem => elem.id === item.id).length > 0}
+                                    >
+                                        {props.type === 'add' ? 'Добавить' : 'Убрать'}
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
+}
 
 export default List
